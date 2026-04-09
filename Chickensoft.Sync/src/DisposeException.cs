0994b2a3bb910133f719a163592120fedb25a7a4
Casey 'Cubusky' Hofland <hofland.casey@gmail.com>
2026-04-09 21:40:25 +0200

    Add DisposalBin

--- Chickensoft.Sync/src/DisposeException.cs
+++ Chickensoft.Sync/src/DisposeException.cs
@@ -4,15 +4,7 @@ using System;
 
 public sealed class DisposeException : Exception
 {
-  public DisposeException()
-  {
-  }
-
-  public DisposeException(string message) : base(message)
-  {
-  }
-
-  public DisposeException(string message, Exception innerException) : base(message, innerException)
-  {
-  }
+  public DisposeException() { }
+  public DisposeException(string message) : base(message) { }
+  public DisposeException(string message, Exception innerException) : base(message, innerException) { }
 }
