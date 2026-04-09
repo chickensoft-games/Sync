9fcb9afdf358f81348dffe59f663b490226aadc0
Casey 'Cubusky' Hofland <hofland.casey@gmail.com>
2026-04-09 21:36:28 +0200

    Add DisposeException

---
+++ Chickensoft.Sync/src/DisposeException.cs
@@ -0,0 +1,18 @@
+namespace Chickensoft.Sync;
+
+using System;
+
+public sealed class DisposeException : Exception
+{
+  public DisposeException()
+  {
+  }
+
+  public DisposeException(string message) : base(message)
+  {
+  }
+
+  public DisposeException(string message, Exception innerException) : base(message, innerException)
+  {
+  }
+}
